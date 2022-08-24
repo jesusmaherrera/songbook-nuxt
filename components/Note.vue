d01903c04ad5d479f40f496fbade7f75de1cc037
Jesus Manuel Herrera Miramontes <jesusmaherrera@gmail.com>
2022-08-23 19:26:13 -0600

    IMPLEMENT SONG VIEWER

---
+++ components/Note.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  value: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <span class="relative">
+    <slot />
+    <span class="absolute -top-5 left-0 text-red-600 text-sm font-semibold">{{
+      value
+    }}</span>
+  </span>
+</template>
--- components/SongViewer.vue
+++ components/SongViewer.vue
@@ -1,51 +1,42 @@
 <template>
-  <div class="bg-white text-gray-800">
-    <div class="pt-4">
-      <span class="relative">
-        R
-        <span class="absolute bottom-3 left-0 text-red-600">F#</span> </span
-      >elative parent
-    </div>
-    <div>
-      Rem Solm Do Fa <br />
-      Un joven se acercó a Jesús que sonreía, <br />
-      La# Solm La7 <br />
-      un joven que quería ser mejor, <br />
-      Rem Solm <br />
-      quiero hacer tu voluntad <br />
-      Do Fa <br />
-      y no sé qué hacer, Señor, <br />
-      La# Solm La7 Rem <br />
-      Tú que eres la verdad, dime, por favor. <br />
-      La7 Rem <br />
-      Si tú quieres ser mi <br />
-      amigo, <br />
-      Do Fa <br />
-      anda y vende lo que tienes <br />
-      Solm Rem <br />
-      y ya libre, ven conmigo <br />
-      La7 Rem yo te <br />
-      ofrezco mucho más. <br />
-      Rem Solm Do Fa <br />
-      Jesús le contempló y era alegre su mirada <br />
-      La# Solm <br />
-      La7 <br />
-      y urgente la llamada y le habló. <br />
-      Rem Solm <br />
-      Pero el joven se marchó <br />
-      Do Fa <br />
-      sin decir apenas nada, <br />
-      La# Solm La7 Rem <br />
-      como el joven era rico, triste se marchó. <br />
-      La7 Rem Si tú <br />
-      quieres ser mi amigo, <br />
-      Do Fa <br />
-      anda y vende lo que tienes <br />
-      Solm Rem <br />
-      y ya libre, ven <br />
-      conmigo <br />
-      La7 Rem <br />
-      yo te ofrezco mucho más. <br />
-    </div>
+  <div class="bg-white text-gray-800 p-2 pt-5">
+    <note value="Rem">U</note>n joven <note value="Solm">s</note>e acercó<note
+      value="Do"
+    />
+    a Jesús qu<note value="Fa" />e sonreía,
+    <Nbr />
+    un jov<note value="La#" />en que quer<note value="Solm">í</note>a ser mejo<note
+      value="La7"
+    />r, <Nbr /> quiero hacer <note value="Rem" />tu voluntad<note value="Solm" />
+    <Nbr /> y no sé<note value="Do" /> qué hacer, Señor,<note value="Fa" /> <Nbr /> Tú que
+    eres <note value="La#" />la verdad, <note value="Solm" />dime, p<note value="La7" />or
+    favor<note value="Rem" />.
+    <Nbr />
+    <Nbr />
+    Si tú quiere<note value="La7" />s ser mi amigo,<note value="Rem" /> <Nbr /> anda y
+    vende <note value="Do" />lo que tienes<note value="Fa" /> <Nbr /> y ya lib<note
+      value="Solm"
+    />re, ven conmig<note value="Rem" />o <Nbr /> yo te ofrez<note value="La7" />co mucho
+    más.<note value="Rem" />
+    <Nbr />
+    <Nbr />
+    Jesús l<note value="Rem" />e conte<note value="Solm" />mpló y era ale<note
+      value="Do"
+    />gre su mirada<note value="Fa" /> <Nbr /> y urgen<note value="La#" />te la llamada y
+    le<note value="Solm" /> habló.<note value="La7" /> <Nbr /> Pero el joven
+    <note value="Rem" />se marchó <note value="Solm" /> <Nbr /> sin deci<note
+      value="Do"
+    />r apenas nada,<note value="Fa" /> <Nbr /> como el j<note value="La#" />oven era
+    rico, t<note value="Solm" />riste se<note value="La7" /> marchó.<note value="Rem" />
+    <Nbr />
+    <Nbr />
+    Si tú quiere<note value="La7" />s ser mi amigo,<note value="Rem" /> <Nbr /> anda y
+    vende <note value="Do" />lo que tienes<note value="Fa" /> <Nbr /> y ya lib<note
+      value="Solm"
+    />re, ven conmig<note value="Rem" />o <Nbr /> yo te ofrez<note value="La7" />co mucho
+    más.<note value="Rem" />
+    <Nbr />
   </div>
 </template>
+
+<style></style>
